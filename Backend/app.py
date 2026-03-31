49a8e9a623109d992b58f7559b1c757cf4f6f41c
Bobby <mrsiddu67@gmail.com>
2026-03-31 14:21:40 +0530

    fixed deployemnet errors

--- Backend/app.py
+++ Backend/app.py
@@ -1,6 +1,10 @@
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import mysql.connector
+
+
+
 
 
 app = Flask(__name__)
@@ -265,6 +269,12 @@ def income():
 # ===============================
 if __name__ == '__main__':
     app.run(debug=True)
+
+import os
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
+
 
 
 
